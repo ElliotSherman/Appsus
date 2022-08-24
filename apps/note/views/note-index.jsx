6afa385315eb1d1e7426f6acf785707e8872e65f
elliot sherman <sheveryosef@gmail.com>
2022-08-24 19:27:17 +0300

    get note bg colors and style grid for note list

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,5 +1,5 @@
+// DONE - gather icons
 // TODO - create following components note-txt/ note-img/ note-video/ noto-todos BONUS: note-audio/ note-canvas/ note-map
-// TODO - create list that renders all the notes using GRID 
 // TODO - the should have to types of list if you have pinned notes they will be rendered at the top the rest of notes will be underneath
 // TODO - support duplicating a note
 
@@ -9,11 +9,12 @@
 // TODO - BONUS: note-audio/ note-canvas/ note-map
 // TODO - allow recording a note!
 
+// in progress - create list that renders all the notes using GRID 
 // in progress - gather assets needed to create app 
 // in progress - gather colors
-// todo - gather icons
 // todo - gather notes[{...}] meta data to render some basic content and skeleton
 import NoteHeader from "../cmps/note-header.jsx"
+import NoteList from "../cmps/note-list.jsx"
 
 export class NoteIndex extends React.Component {
     render() {
@@ -22,6 +23,7 @@ export class NoteIndex extends React.Component {
                 {/* Keep */}
                 {/* <i class="fa-solid fa-magnifying-glass" style={{color:'red'}}></i> */}
                 <NoteHeader />
+                <NoteList />
             </div>
         )
     }
