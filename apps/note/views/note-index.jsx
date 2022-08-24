d9f87e8efaaf52c52c75cc4bad40d2bad8b5f2ca
elliot sherman <sheveryosef@gmail.com>
2022-08-24 12:42:05 +0300

    create list of targets for workflow

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,8 +1,25 @@
+// TODO - create following components note-txt/ note-img/ note-video/ noto-todos BONUS: note-audio/ note-canvas/ note-map
+// TODO - create list that renders all the notes using GRID 
+// TODO - the should have to types of list if you have pinned notes they will be rendered at the top the rest of notes will be underneath
+// TODO - support duplicating a note
+
+// TODO - App integration - allow a note to be sent directly to the compose email messge page using query params!
+// DONE BONUSES
+// TODO - support drag and drop functionality
+// TODO - BONUS: note-audio/ note-canvas/ note-map
+// TODO - allow recording a note!
+
+// in progress - gather assets needed to create app 
+// in progress - gather colors
+// todo - gather icons
+// todo - gather notes[{...}] meta data to render some basic content and skeleton
 
 export class NoteIndex extends React.Component {
     render() {
         return (
-            <div>note app</div>
+            <div className="note-app">
+                
+            </div>
         )
     }
 }
