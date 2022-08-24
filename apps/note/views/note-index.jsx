a34eee5b1fa83600299fc980e5042a0ee015c908
elliot sherman <sheveryosef@gmail.com>
2022-08-24 20:34:06 +0300

    build database for rendering notes

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,17 +1,18 @@
 // DONE - gather icons
-// TODO - create following components note-txt/ note-img/ note-video/ noto-todos BONUS: note-audio/ note-canvas/ note-map
-// TODO - the should have to types of list if you have pinned notes they will be rendered at the top the rest of notes will be underneath
-// TODO - support duplicating a note
+// DONE - create list that renders all the notes using GRID 
+// DONE - gather assets needed to create app 
+// DONE - gather colors
+// DONE - create following components note-txt/ note-img/ note-video/ noto-todos 
 
+// TODO - BONUS: note-audio/ note-canvas/ note-map
+// TODO - there should be two types of lists if you have pinned notes they will be rendered at the top and the rest of notes will be underneath
+// TODO - support duplicating a note
 // TODO - App integration - allow a note to be sent directly to the compose email messge page using query params!
-// DONE BONUSES
+// ? BONUSES ================
 // TODO - support drag and drop functionality
 // TODO - BONUS: note-audio/ note-canvas/ note-map
 // TODO - allow recording a note!
 
-// in progress - create list that renders all the notes using GRID 
-// in progress - gather assets needed to create app 
-// in progress - gather colors
 // todo - gather notes[{...}] meta data to render some basic content and skeleton
 import NoteHeader from "../cmps/note-header.jsx"
 import NoteList from "../cmps/note-list.jsx"
@@ -19,9 +20,7 @@ import NoteList from "../cmps/note-list.jsx"
 export class NoteIndex extends React.Component {
     render() {
         return (
-            <div className="note-app" style={{fontFamily:'product-sans' , fontSize:'22px',color:'var(--font-clr)'}}>
-                {/* Keep */}
-                {/* <i class="fa-solid fa-magnifying-glass" style={{color:'red'}}></i> */}
+            <div className="note-app" >
                 <NoteHeader />
                 <NoteList />
             </div>
